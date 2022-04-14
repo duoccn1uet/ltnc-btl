08938ec8c51af730986b0e7b984627f13a61f7fa
hduoc2003 <21020059@vnu.edu.vn>
2022-04-14 22:07:05 +0700

    ff

--- Doodle_Jump/main.cpp
+++ Doodle_Jump/main.cpp
@@ -50,7 +50,7 @@ int main(int agrc, char* argv[])
 {
     srand(time(0));
     initSDL(window, renderer);
-    game.ShowMenu(renderer, FONT_FOLDER + MAP_NAME + OPTION_FONT, MENU_FOLDER + MAP_NAME + "menu.png");
+    ///game.ShowMenu(renderer, FONT_FOLDER + MAP_NAME + OPTION_FONT, MENU_FOLDER + MAP_NAME + "menu.png");
    ///Khi thông thường chạy với môi trường bình thường ở nhà
    /// SDL_RenderPresent(renderer);
    ///Khi chạy ở máy thực hành WinXP ở trường (máy ảo)
